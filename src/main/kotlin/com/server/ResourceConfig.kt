50b482267d9b68579fac0bb712e0d4401e62520e
Chami Rachid <chamirachid1@gmail.com>
2020-06-13 17:30:58 -0700

    Adds package name classes registration

--- src/main/kotlin/com/server/ResourceConfig.kt
+++ src/main/kotlin/com/server/ResourceConfig.kt
@@ -6,6 +6,9 @@ import org.glassfish.jersey.server.ResourceConfig
 class ResourceConfig :  ResourceConfig(){
 
     init {
-        register(BaseDummyImpl::class.java)
+//        register(BaseDummyImpl::class.java) : To register the class directly
+//        packages("com") : To register using package name (dont forget to move the @path from the interface of the `BaseDummyApi` to the implementation
+
+
     }
 }
