b1ec984d0111eea0529304b162039774ba592f9e
Chami Rachid <chamirachid1@gmail.com>
2020-06-14 05:10:04 -0700

    Adds integration test

--- src/main/kotlin/server/ResourceConfig.kt
+++ src/main/kotlin/server/ResourceConfig.kt
@@ -1,18 +1,19 @@
 package server
 
+import api.com.BaseDummyImpl
 import org.glassfish.jersey.server.ResourceConfig
 
 class ResourceConfig :  ResourceConfig(){
 
     init {
-//        register(BaseDummyImpl::class.java) // To register the class directly
+        register(BaseDummyImpl::class.java) // To register the class directly
 //        packages("com") // To register using package name (dont forget to move the @path from the interface of the `BaseDummyApi` to the implementation
-        registerUsingSPI() // Registers classes using a Service Provider Interface
+//        registerUsingSPI() // Registers classes using a Service Provider Interface
     }
 
-    private fun registerUsingSPI() {
-        BaseDummyLoader.get().forEach {
-            register(it)
-        }
-    }
+//    private fun registerUsingSPI() {
+//        BaseDummyLoader.get().forEach {
+//            register(it)
+//        }
+//    }
 }
---
+++ src/test/java/integrationTest/DummyTest.kt
@@ -0,0 +1,36 @@
+package integrationTest
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.glassfish.jersey.test.JerseyTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import server.ResourceConfig
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.core.Application
+
+
+class DummyTest : JerseyTest() {
+
+    val testClient = ClientBuilder.newClient()
+
+    @BeforeEach
+    override fun setUp() {
+        super.setUp()
+    }
+
+    @AfterEach
+    override fun tearDown() {
+        super.tearDown()
+    }
+
+    override fun configure(): Application {
+        return ResourceConfig()
+    }
+
+    @Test
+    fun `listing test`() {
+        assert(target("/dum/dummies").request().get().readEntity(String::class.java) == "dummy, anotherDummy")
+    }
+}
